26ef0c6b2a609b8cb81fc3464b9e903de692460c
Harrison Bowlby <hrbowlby@gmail.com>
2021-10-22 20:05:50 -0700

    enhancement 04

--- phpmotorstest2/view/template.php
+++
@@ -1,8 +0,0 @@
-<?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/module/header.php'; ?>
-            <nav>
-                <?php echo $navList; ?>
-            </nav>
-            <main>
-                <p>Content here</p>
-            </main>
-<?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/module/footer.php'; ?> 
